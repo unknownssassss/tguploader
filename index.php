33d5ae9e6c3fe22e60a1edd8cc89e3b5919a1e29
unknownssassss <mahdi.khaleghi1234@gmail.com>
2020-08-17 15:02:10 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
 
-use Mhor\MediaInfo\MediaInfo;
+/*use Mhor\MediaInfo\MediaInfo;*/
 try{
-$mediaInfo = new MediaInfo();
+/*$mediaInfo = new MediaInfo();
 $mediaInfoContainer = $mediaInfo->getInfo('https://mytgup.herokuapp.com/AOyIzN4YzN5UTM/406847488/_%D9%85%D8%A8%D8%A7%D8%B4%D8%B1_%D9%85%D8%B9_%D8%AD%D8%B4%D9%8A%D8%B4_%D9%88_%D9%87%D8%AA%D9%84%D8%B1_%D9%88_%D9%81%D8%B1%D9%8A%D8%B2%D8%B1_%F0%9F%94%A5_THE_7H_LEGENDS_ARE_BACK_5314349956612491451.mp4');
-print_r($mediaInfoContainer);
+print_r($mediaInfoContainer);*/
+$exec = exec('MediaInfo --help', $output);
+print_r($output);
 }catch(exception $e){
 echo "boom";
 }
