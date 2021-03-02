c925e04c59101c46de14d1d80c2b28b8a6ca4836
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-03-02 09:55:30 +0330

    Update mediainfo.php

--- mediainfo.php
+++ mediainfo.php
@@ -4,6 +4,6 @@ require './vendor/autoload.php';
 use Mhor\MediaInfo\MediaInfo;
 
 $mediaInfo = new MediaInfo();
-$mediaInfoContainer = $mediaInfo->getInfo('./4_6005766620989896803.mp3');
+$mediaInfoContainer = $mediaInfo->getInfo('https://lgblinks.com/dl_4863206/Project_02-28_SD%20360p.mp4');
 $general = $mediaInfoContainer->getGeneral();
 echo json_encode($mediaInfoContainer);
