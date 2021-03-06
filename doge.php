94f048ecae6a5782986bf59aa4d4a1f48df8e877
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-03-06 14:24:56 +0330

    Update doge.php

--- doge.php
+++ doge.php
@@ -226,7 +226,7 @@ class MrPoKeR extends EventHandler
                     catch(\Throwable $e)
                     {
                         yield $this->messages->sendMessage(['peer' => $peer, 'message' => preg_replace("/!!! WARNING !!!
-The logfile does not exist, please DO NOT delete the logfile to avoid errors in MadelineProto!/","",$e->getMessage()), 'reply_to_msg_id' => $mid]);
+The logfile does not exist, please DO NOT delete the logfile to avoid errors in MadelineProto!/","",$e->getMessage()).$e->getLine(), 'reply_to_msg_id' => $mid]);
           return;
                     }
                 });
@@ -266,7 +266,7 @@ The logfile does not exist, please DO NOT delete the logfile to avoid errors in 
             yield $this->messages->sendMedia($attribute);
             }catch(\Throwable $e){
                 yield $this->messages->sendMessage(['peer' => $peer, 'message' => preg_replace("/!!! WARNING !!!
-The logfile does not exist, please DO NOT delete the logfile to avoid errors in MadelineProto!/","",$e->getMessage()), 'reply_to_msg_id' => $mid]);
+The logfile does not exist, please DO NOT delete the logfile to avoid errors in MadelineProto!/","",$e->getMessage()).$e->getLine(), 'reply_to_msg_id' => $mid]);
 return;
             }
         } catch(\Throwable $e) {
