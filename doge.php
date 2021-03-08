8c2cadd544615a238229e03505b0086e7054403e
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-03-08 21:21:15 +0330

    Update doge.php

--- doge.php
+++ doge.php
@@ -1,7 +1,7 @@
 <?php
 set_time_limit(0);
 ini_set('memory_limit', '512M');
-ini_set('max_execution_time', 800);
+ini_set('max_execution_time',-1); 
 date_default_timezone_set("Asia/tehran");
 if (!\file_exists('madeline.php')) {
     \copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
