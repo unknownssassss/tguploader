7373280359feba7ac0fb4f33460a541f23eeb04d
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-03-09 19:06:11 +0330

    Update doge.php

--- doge.php
+++ doge.php
@@ -1,6 +1,6 @@
     <?php
     set_time_limit(0);
-    ini_set('memory_limit', '2G');
+    ini_set('memory_limit', '512M');
     ini_set('max_execution_time',-1); 
     date_default_timezone_set("Asia/tehran");
     if (!\file_exists('madeline.php')) {
@@ -387,7 +387,7 @@
                 $peer = $getallinfo['bot_api_id'];
                 if($message == "members" && yield $this->Is_Mod($from_id)){
                     $mems = yield $this->getAllGroups('users');
-                    yield $this->messages->sendMessage(['peer' => $peer, 'message' => count($mems)]);
+                    yield $this->messages->sendMessage(['peer' => $peer, 'message' =>"Members : ".count($mems)]);
                     unset($mems);
                     return;
                 }
