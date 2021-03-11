f21e0054bbac26711b971944542d32f4b197f5f2
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-03-11 12:32:38 +0330

    Update mediainfo.php

--- mediainfo.php
+++ mediainfo.php
@@ -2,32 +2,69 @@
 set_time_limit(0);
 ini_set('memory_limit', -1);
 ini_set('max_execution_time', -1);
-function pingD($domain){
-
-try{
-$domain = explode(":",$domain);
-    $starttime = microtime(true);
-    $file      = fsockopen ($domain[0], $domain[1], $errno, $errstr, 10);
-    $stoptime  = microtime(true);
-    $status    = 0;
-
-    if (!$file){
-return "done : ".$domain[0].":".$domain[1];
-}
-    else {
-        fclose($file);
-        $status = ($stoptime - $starttime) * 1000;
-        $status = floor($status);
+function fetchdata($channel, $postid, $proxy) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_URL, "https://t.me/$channel/$postid?embed=1");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    $result = curl_exec($ch);
+    $m = explode('data-view="', $result);
+    if (!isset($m[1])) {
+        return false;
     }
-    return $status."-".$domain[0].":".$domain[1];
-}catch(Error $e){
-return $e->getMessage();
+    $m = explode('"', $m[1]);
+    if (!isset($m[0])) {
+        return false;
+    }
+    $array = [];
+    $array =
+    [
+        'key' => $m[0]
+    ];
+    preg_match_all("/Set\-Cookie\:(.*)/", $result, $cook);
+    if (!isset($cook[1])) {
+        return false;
+    }
+    $cook = explode(";", implode("\n", array_values($cook[1])));
+    if (!isset($cook[0])) {
+        return false;
+    }
+    $array =
+    [
+        'cookie' => $cook[0]
+    ];
+    return $array;
 }
-}
-$list = file_get_contents("IpTester");
-$list = explode("\n",$list);
-foreach($list as $proxy){
-echo pingD($proxy).PHP_EOL;
+function addViewToPost($channel, $postid, $key, $cookie, $proxy) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_URL, "https://t.me/$channel/$postid?embed=1&view=$key");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['x-requested-with: XMLHttpRequest', 'user-agent: Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0', 'referer: https://t.me/'.$channel.'/'.$postid.'?embed=1', 'cookie: '.$cookie]);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    $result = curl_exec($ch);
+    return $result;
 }
 
-    
+function run($channel, $postid, $proxy) {
+    $fetch = fetchdata($channel, $postid, $proxy);
+    if (is_array($fetch) && isset($fetch['key']) && isset($fetch['cookie'])) {
+        $result = addViewToPost($channel, $postid, $fetch['key'], $fetch['cookie'], $proxy);
+        if ($result) {
+            return "proxy $proxy finished its job successfully";
+        } else {
+            return 'Thread with proxy '.$proxy.' has been terminated';
+    }
+} else {
+    return 'cant Fetch Data '.$proxy;
+}
+}
+$list = explode("\n",file_put_contents("IpTester"));
+foreach($list as $proxy){
+    echo run("skyteam",430,$proxy).PHP_EOL;
+}
