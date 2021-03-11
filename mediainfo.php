43f75fc7ad80a8a64c60626e7311de4ccb828b13
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-03-11 13:32:23 +0330

    Update mediainfo.php

--- mediainfo.php
+++ mediainfo.php
@@ -11,6 +11,10 @@ function fetchdata($channel, $postid, $proxy) {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_PROXY, $proxy);
     $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo curl_error($ch).PHP_EOL;
+    }
+    curl_close($ch);
     $m = explode('data-view="', $result);
     if (!isset($m[1])) {
         return false;
@@ -19,7 +23,7 @@ function fetchdata($channel, $postid, $proxy) {
     if (!isset($m[0])) {
         return false;
     }
- 
+    echo "get Key from url ".$m[1];
     $array = [];
     $array['key'] = $m[0];
     preg_match_all("/Set\-Cookie\:(.*)/", $result, $cook);
@@ -30,37 +34,39 @@ function fetchdata($channel, $postid, $proxy) {
     if (!isset($cook[0])) {
         return false;
     }
+    echo "Get Cookie From Url ".$cook[0];
     $array['cookie'] = $cook[0];
-    
+
     return $array;
 }
-function addViewToPost($channel, $postid, $key, $cookie, $proxy) {
+function addViewToPost($key, $cookie, $proxy) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
     curl_setopt($ch, CURLOPT_TIMEOUT, 10);
     curl_setopt($ch, CURLOPT_HEADER, true);
-    curl_setopt($ch, CURLOPT_URL, "https://t.me/$channel/$postid?embed=1&view=$key");
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['x-requested-with: XMLHttpRequest', 'user-agent: Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0', 'referer: https://t.me/'.$channel.'/'.$postid.'?embed=1', 'cookie: '.$cookie]);
+    curl_setopt($ch, CURLOPT_URL, "https://t.me/v/?views=$key");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-Requested-With: XMLHttpRequest', 'User-Agent: Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0', 'Referer: https://t.me/'.$channel.'/'.$postid.'?embed=1', 'Cookie: '.$cookie]);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_PROXY, $proxy);
     $result = curl_exec($ch);
+    curl_close($ch);
     return $result;
 }
 
 function run($channel, $postid, $proxy) {
     $fetch = fetchdata($channel, $postid, $proxy);
     if (is_array($fetch) && isset($fetch['key']) && isset($fetch['cookie'])) {
-        $result = addViewToPost($channel, $postid, $fetch['key'], $fetch['cookie'], $proxy);
+        $result = addViewToPost($fetch['key'], $fetch['cookie'], $proxy);
         if ($result) {
             return "proxy $proxy finished its job successfully";
         } else {
             return 'Thread with proxy '.$proxy.' has been terminated';
+        }
+    } else {
+        return 'cant Fetch Data '.$proxy;
     }
-} else {
-    return 'cant Fetch Data '.$proxy;
 }
-}
-$list = explode("\n",file_get_contents("IpTester"));
-foreach($list as $proxy){
-    echo run("skyteam",430,$proxy).PHP_EOL;
+$list = explode("\n", file_get_contents("IpTester"));
+foreach ($list as $proxy) {
+    echo run("skyteam", 430, $proxy).PHP_EOL;
 }
