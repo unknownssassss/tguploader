a4281efcdcf09c21afdc50070837c87069e6924a
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-04-14 11:29:17 +0430

    Update index.php

--- index.php
+++ index.php
@@ -3,33 +3,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
-    <div>
-        <?php
-        if (isset($_POST['link']) && isset($_POST['f'])) {
-            include "vendor/autoload.php";
-            $client = new GuzzleHttp\Client();
-            $client->request(
-                'GET',
-                urldecode($_POST['link']),
-                array('sink' => $_POST['f']),
-                [
-                    'progress' => function(
-                        $downloadTotal,
-                        $downloadedBytes,
-                        $uploadTotal,
-                        $uploadedBytes
-                    ) {
-                        echo "<pre>";
-                        echo $downloadedBytes."<br>";
-                        echo "</pre>";
-                    },
-                ]
-            );
-
-        }
-        ?>
-    </div>
-    <form action="" method="post" class="myForm">
+    <form action="dl.php" method="post" class="myForm">
         <textarea name="link"></textarea>
         <input type="text" name="f" />
         <input type="submit" value="Download" id="Sub" />
@@ -39,10 +13,9 @@
     </div>
 <script type="text/javascript" charset="utf-8">
             $("#Sub").click(function(e){
-alert("test");
         $.ajax({
             type: "POST",
-            url: "index.php",
+            url: "dl.php",
             data: $(".myForm").serialize(),
             success: function(response){
                 $(".result").html(response);
@@ -50,38 +23,5 @@ alert("test");
         });
     })
         </script>
-    <?php
-    function formatBytes($bytes, $precision = 2) {
-
-        $units = ['B',
-            'KB',
-            'MB',
-            'GB',
-            'TB',
-            'PB',
-            'EB',
-            'ZB',
-            'YB'];
-        $bytes = max($bytes, 0);
-        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-        $pow = min($pow, count($units) - 1);
-        $bytes /= pow(1024, $pow);
-
-        return round($bytes, $precision) . ' ' . $units[$pow];
-    }
-    if (isset($_GET['type'])) {
-        if ($_GET['type'] == "scan") {
-            $scan = scandir(".");
-            foreach ($scan as $file) {
-                if (is_file($file)) {
-                    echo "<pre>";
-                    echo $file ." = ".formatbytes(filesize($file))."<br>";
-                    echo "</pre>";
-                }
-            }
-        }
-    }
-    ?>
-
 </body>
 </html>
