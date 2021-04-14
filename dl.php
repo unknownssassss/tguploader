433f3f6470afbab5b633a7d5c9e5894c70835c80
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-04-14 11:30:03 +0430

    Create dl.php

---
+++ dl.php
@@ -0,0 +1,55 @@
+<?php
+        if (isset($_POST['link']) && isset($_POST['f'])) {
+            include "vendor/autoload.php";
+            $client = new GuzzleHttp\Client();
+            $client->request(
+                'GET',
+                urldecode($_POST['link']),
+                array('sink' => $_POST['f']),
+                [
+                    'progress' => function(
+                        $downloadTotal,
+                        $downloadedBytes,
+                        $uploadTotal,
+                        $uploadedBytes
+                    ) {
+                        echo "<pre>";
+                        echo $downloadedBytes."<br>";
+                        echo "</pre>";
+                    },
+                ]
+            );
+        }
+   
+    if (isset($_GET['type'])) {
+function formatBytes($bytes, $precision = 2) {
+
+        $units = ['B',
+            'KB',
+            'MB',
+            'GB',
+            'TB',
+            'PB',
+            'EB',
+            'ZB',
+            'YB'];
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+        if ($_GET['type'] == "scan") {
+            $scan = scandir(".");
+            foreach ($scan as $file) {
+                if (is_file($file)) {
+                    echo "<pre>";
+                    echo $file ." = ".formatbytes(filesize($file))."<br>";
+                    echo "</pre>";
+                }
+            }
+        }
+    }
+    ?>
+        ?>
