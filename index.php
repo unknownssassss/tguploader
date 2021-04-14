ad07584a2c5c3447a3c47ac7b4a77686e2eabc30
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-04-14 10:15:52 +0430

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
     <div>
         <?php
         if (isset($_POST['link']) && isset($_POST['f'])) {
-           var_dump($_POST);
+           include "vendor/autoload.php";
+           $client = new GuzzleHttp\Client();
+           $client->request(
+  'GET',
+  urldecode($_POST['link']),
+  array('sink' => $_POST['f'])
+);
             return;
         }
         ?>
@@ -14,13 +20,37 @@
     </form>
 </body>
 </html>
-/*if(isset($_GET['type'])){
+<?php
+function formatBytes($bytes, $precision = 2) {
+
+        $units = ['B',
+            'KB',
+            'MB',
+            'GB',
+            'TB',
+            'PB',
+            'EB',
+            'ZB',
+            'YB'];
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        $bytes /= pow(1024, $pow);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+if(isset($_GET['type'])){
 if($_GET['type'] == "scan"){
+$scan = scandir(".");
+foreach($scan as $file){
+if(is_file($file)){
 echo "<pre>";
-print_r(scandir("."));
+echo $file ." = ".formatbytes(filesize($file))."<br>";
 echo "</pre>";
 }
-if($_GET['type'] == "dl" && isset($_GET['url']) && isset($_GET['f'])){
+}
+}
+/*if($_GET['type'] == "dl" && isset($_GET['url']) && isset($_GET['f'])){
 include "vendor/autoload.php";
 $client = new GuzzleHttp\Client();
 $client->request(
@@ -30,4 +60,4 @@ $client->request(
 );
 }
 }
-*/
+*/?>
