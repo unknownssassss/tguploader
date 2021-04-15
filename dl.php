72bc34e022f86aef322d872ff3047184b70f9195
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-04-15 21:02:55 +0430

    Update dl.php

--- dl.php
+++ dl.php
@@ -34,8 +34,9 @@ die;
     if(isset($_FILES['fileToUpload'])){
         $inputfile = microtime(true)."_".basename($_FILES['fileToUpload']['name']);
 if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$inputfile)){
- $outputimage = "output.png";
-        exec("ffmpeg -i $inputfile -ss 00:00:01.000 -v frames 1 $outputimage",$output);
+ $outputimage = "output.jpg";
+exec("ffmpeg -ss 3 -i $inputfile -vf "select=gt(scene\,0.4)" -frames:v 5 -vsync vfr -vf fps=fps=1/600 $outputimage",$output);
+       // exec("ffmpeg -i $inputfile -ss 00:00:01.000 -v frames 1 $outputimage",$output);
 print_r($output);
 return;
 }
