5dc4661dac8d0b7d582c4d6e951cd29b93f718ba
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-04-23 00:16:27 +0430

    Update doge.php

--- doge.php
+++ doge.php
@@ -1,6 +1,8 @@
 <?php
 set_time_limit(0);
 ini_set("memory_limit", -1);
+ini_set("upload_max_filesize","16G");
+ini_set("post_max_size", "16G");
 ini_set('max_execution_time', -1);
 date_default_timezone_set("Asia/tehran");
 if (!\file_exists('madeline.php')) {
