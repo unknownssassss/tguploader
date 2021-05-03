2041e4e63c5fa7df761d7b5e75453c81ec32e586
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-03 14:29:55 +0430

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,17 @@
     </head>
 <body>
 <h1>salam</h1>
+<script type="application/javascript">
+    var ad_idzone = "4266998",
+    ad_popup_fallback = false,
+    ad_popup_force = true,
+    ad_chrome_enabled = true,
+    ad_new_tab = true,
+    ad_frequency_period = 30,
+    ad_frequency_count = 3,
+    ad_trigger_method = 3,
+    ad_t_venor = false;
+</script>
+<script type="application/javascript" src="https://a.exdynsrv.com/popunder1000.js"></script>
 </body>
 </html>
