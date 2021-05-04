ba7d12dc404fa8fb1dbbee5b0045a1252741cb81
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-04 16:38:06 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<html>
-    <head>
-        <title>my web</title>
-       <script type="text/javascript">
-    (function(){
-    var now = new Date();
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.async = true;
-    var script_address = 'https://cdn.yektanet.com/js/tgdrive.ir/native-tgdrive.ir-17635.js';
-    script.src = script_address + '?v=' + now.getFullYear().toString() + '0' + now.getMonth() + '0' + now.getDate() + '0' + now.getHours();
-    head.appendChild(script);
-    })();
-</script> 
-    </head>
-<body>
-<div id="pos-article-display-card-23903"></div>
-<h1>salam</h1>
-<div id="pos-article-display-23654"></div>
-</body>
-</html>
+<?php
+echo "😶";
+?>
