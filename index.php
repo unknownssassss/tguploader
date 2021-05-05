6fa9211f1cca4d79301cd8cb6ca3690087ef856c
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-05 09:38:50 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo "😶";
-?>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="a.validate.01" content="9104fe62dc9a70bb452f34ffec63c51d89ae" />
+        <title>Hii</title>
+        
+    </head>
+    <body>
+        😕
+    </body>
+</html>
