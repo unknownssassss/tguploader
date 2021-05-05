dae5d3da10293925439d666ce48b6d3c291406a3
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-05 09:45:07 +0430

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta name="a.validate.01" content="9104fe62dc9a70bb452f34ffec63c51d89ae" />
         <title>Hii</title>
-        <script type="text/javascript" data-adel="atag" src="//acdcdn.com/script/atg.js" czid="agv3lstu"></script>
-    </head>
+         </head>
     <body>
         😕
+<script type="text/javascript" data-adel="atag" src="//acdcdn.com/script/atg.js" czid="agv3lstu"></script>
     </body>
 </html>
