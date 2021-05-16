051b5d202c20db7a8deed737fdb5aaf3a979c721
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-16 09:09:35 +0430

    Update api.py

--- api/api.py
+++ api/api.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
