35db4226dc6881f30f213413684502ffa6ad8e96
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-17 15:08:06 +0430

    Update index.php

--- test/index.php
+++ test/index.php
@@ -1,5 +1,8 @@
 <?php
-if(file_exists("vendor/autoload.php")){
+if(file_exists("../vendor/autoload.php")){
+require "../vendor/autoload.php";
+use \danog\MadelineProto\Tools;
+var_dump(get_class_methods('Tools'));
 echo "has";
 }else{
 echo "nis";
