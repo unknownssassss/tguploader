d31e16a563e7e13e51b546bd6b4d5e82d9948e38
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-07-08 02:16:06 +0430

    Update doge.php

--- doge.php
+++ doge.php
@@ -469,12 +469,15 @@ class MrPoKeR extends EventHandler
                 while (null !== $chunk = yield $stream->read()) {
                     if(preg_match_all("#\[download\]\s+(?<percentage>\d+(?:\.\d+)?%)\s+of\s+(?<size>[~]?\d+(?:\.\d+)?(?:K|M|G)iB)(?:\s+at\s+(?<speed>(\d+(?:\.\d+)?(?:K|M|G)iB/s)|Unknown speed))?(?:\s+ETA\s+(?<eta>([\d:]{2,8}|Unknown ETA)))?(\s+in\s+(?<totalTime>[\d:]{2,8}))?#i",$chunk,$res,PREG_SET_ORDER)){
                        foreach($res as $result) {
-                           $prog = $result['percentage'] ?? 0;
+                  yield $this
+                    ->messages
+                    ->editMessage(['peer' => $peer, 'message' => "testtttt", 'id' => $id, 'parse_mode' => "MarkDown"]);  
+                      /*     $prog = $result['percentage'] ?? 0;
                            if($prog % 5 == 0){
                                    yield $this
                     ->messages
                     ->editMessage(['peer' => $peer, 'message' => "testtttt", 'id' => $id, 'parse_mode' => "MarkDown"]);  
-                           }
+                           }*/
                        }
                     }
                 }
