1af566b3597db3dac88fae520670598f7a39ab4f
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-10-21 21:58:01 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
-<html>
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <meta name="a.validate.01" content="9104fe62dc9a70bb452f34ffec63c51d89ae" />
-        <title>Hii</title>
-         </head>
-    <body>
-        <a href="https://t.me/skyteam">Sky team channel</a>
-<script data-cfasync="false" type="text/javascript" src="https://www.onclickperformance.com/a/display.php?r=4481407"></script>          
-<script type="text/javascript" data-adel="atag" src="//acacdn.com/script/atg.js" czid="7jyeb9qb"></script>                
-<script type="text/javascript" data-adel="atag" src="//acacdn.com/script/atg.js" czid="sxmsosta"></script>        
-<script type="text/javascript" data-adel="atag" src="//acdcdn.com/script/atg.js" czid="agv3lstu"></script>
-    </body>
-</html>
+<?php
+$updates = file_get_contents("php://input");
+if(empty($updates))
+{
+return;
+}
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,            "http://mahdi-kh.ir/neenja/Neenja.php");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt($ch, CURLOPT_POST,           1 );
+curl_setopt($ch, CURLOPT_POSTFIELDS,     $updates); 
+curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: application/x-www-form-urlencoded'));
+$result=curl_exec ($ch);
