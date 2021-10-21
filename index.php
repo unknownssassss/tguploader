58ee5af277d2f699c22a90c818812fa7481e75d0
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-10-21 22:07:02 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+set_time_limit(0);
+ini_set('memory_limit', '512M');
+ini_set('max_execution_time', 800);
 $updates = file_get_contents("php://input");
 if(empty($updates))
 {
@@ -6,7 +9,6 @@ return;
 }
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,            "http://mahdi-kh.ir/neenja/Neenja.php");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
 curl_setopt($ch, CURLOPT_POST,           1 );
 curl_setopt($ch, CURLOPT_POSTFIELDS,     $updates); 
 curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: application/x-www-form-urlencoded'));
